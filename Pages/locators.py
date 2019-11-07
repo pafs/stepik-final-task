143f26318bdc747b26ed3ec770c8297524acd2bf
pafs <i.sintsov@gmail.com>
2019-11-07 16:01:13 +0300

    Added login page support

--- Pages/locators.py
+++ Pages/locators.py
@@ -1,5 +1,15 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators:
+    LOGIN_EMAIL = (By.NAME, "login-username")
+    LOGIN_PASSWORD = (By.NAME, "login-password")
+    REGISTER_EMAIL = (By.NAME, "registration-email")
+    REGISTER_PASSWORD = (By.NAME, "registration-password1")
+    REGISTER_PASSWORD_CONFIRM = (By.NAME, "registration-password2")
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
+
