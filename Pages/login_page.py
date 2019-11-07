8d547419e9880ed7d048f24158d820565f3703ee
pafs <i.sintsov@gmail.com>
2019-11-07 16:11:07 +0300

    Fixed an issue

--- Pages/login_page.py
+++ Pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.browser.current_url(), "The URL is missing 'login' - might be wrong URL"
+        assert "login" in self.browser.current_url, "The URL is missing 'login' - might be wrong URL"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present"
